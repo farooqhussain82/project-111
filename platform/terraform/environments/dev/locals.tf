06f64c7c5cecbf04d96c8601cd25f16d9f6134ac
Farooq Hussain <mine.farooq@gmail.com>
2023-06-14 17:43:17 +0500

    Update locals.tf

--- platform/terraform/environments/dev/locals.tf
+++ platform/terraform/environments/dev/locals.tf
@@ -1,3 +1,22 @@
-locals {
+data "aws_ssm_parameter" "private_subnets" {
+  name = "/aft/provisioned/vpc/private_subnets"
+}
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/aft/provisioned/vpc/vpc_id"
+}
+
+data "aws_ssm_parameter" "vpc_cidr" {
+  name = "/aft/provisioned/vpc/vpc_cidr"
+}
+
+data "aws_ssm_parameter" "database_subnet_group" {
+  name = "/aft/provisioned/vpc/database_subnet_group"
+}
+
+locals {
+  private_subnets = split(",", data.aws_ssm_parameter.private_subnets)
+  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_cidr = data.aws_ssm_parameter.vpc_cidr
+  database_subnet_group = data.aws_ssm_parameter.database_subnet_group
 }
